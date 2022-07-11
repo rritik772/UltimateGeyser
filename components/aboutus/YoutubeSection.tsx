669554cec61c819f3ad8c016000c81c4d08a743c
ritik ranjan <rritik772@outlook.com>
2022-07-11 21:03:04 +0530

    reponsive youtube

--- components/aboutus/YoutubeSection.tsx
+++ components/aboutus/YoutubeSection.tsx
@@ -7,24 +7,21 @@ const YoutubeSection = () => {
                 <span className='font-bold uppercase tracking-wide text-4xl text-red-500 underline-black' id="out-youtube">Our Youtube</span>
                 <span className='mt-5 font-xl uppercase tracking-widest text-center'>Find More about us</span>
             </section>
-            <section className='flex justify-center gap-5 flex-wrap'>
+            <section className='flex justify-center gap-5 flex-wrap p-5'>
                 <div className='border shadow rounded-lg overflow-hidden'>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/u6NOTogsw8M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe  src="https://www.youtube.com/embed/u6NOTogsw8M" className="<md:w-[20rem] w-[30rem] h-[25rem]" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                 <div className='border shadow rounded-lg overflow-hidden'>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/u6NOTogsw8M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe  src="https://www.youtube.com/embed/u6NOTogsw8M" className="<md:w-[20rem] w-[30rem] h-[25rem]" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                 <div className='border shadow rounded-lg overflow-hidden'>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/u6NOTogsw8M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe  src="https://www.youtube.com/embed/u6NOTogsw8M" className="<md:w-[20rem] w-[30rem] h-[25rem]" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                 <div className='border shadow rounded-lg overflow-hidden'>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/u6NOTogsw8M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe  src="https://www.youtube.com/embed/u6NOTogsw8M" className="<md:w-[20rem] w-[30rem] h-[25rem]" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                 <div className='border shadow rounded-lg overflow-hidden'>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/u6NOTogsw8M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-                <div className='border shadow rounded-lg overflow-hidden'>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/u6NOTogsw8M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe  src="https://www.youtube.com/embed/u6NOTogsw8M" className="<md:w-[20rem] w-[30rem] h-[25rem]" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </section>
         </div>
